142a9171713655aba239ada44289d7e65e31ddee
Marcelo da Silva Cruz <mcruz@uesc.br>
2023-10-06 13:39:21 -0300

    Commit implementação de codigo

--- Exercicio5.cpp
+++ Exercicio5.cpp
@@ -12,4 +12,39 @@ struct Voo {
     int horaSaida;
     int horaChegada;
     int horaEmbarque;
+
+
+    int calcularHoraPartida() {
+        // Implementar codigo
+        
+    }
+
+    bool estaAtrasado() {
+        // Implementar codigo
+        
+    }
+
+    void exibirInformacoes() {
+        // Implementar codigo
+
+    }
 };
+
+int main() {
+    
+    Voo meuVoo;
+    meuVoo.numVoo = 123;
+    meuVoo.origem = "New York";
+    meuVoo.destino = "Los Angeles";
+    meuVoo.tempoVoo = 300;  
+    meuVoo.horaSaida = 1000; 
+    meuVoo.horaChegada = 1500; 
+    meuVoo.horaEmbarque = 930; 
+
+    // Implementar codigo
+
+
+
+    
+    return 0;
+}
